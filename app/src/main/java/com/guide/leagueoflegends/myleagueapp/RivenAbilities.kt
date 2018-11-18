e6db8dd06a5d2569c8480b0599931228d3587901
Cooper, Alexander F <alexander-f-cooper@uiowa.edu>
2018-11-18 10:32:46 -0600

    I added the 3 fragments and created a tablayout for riven's abilities. So far there are 3 tabs, they just say first, second, third tab respectively. Also created MyPagerAdapter.kt class.

--- app/src/main/java/com/guide/leagueoflegends/myleagueapp/RivenAbilities.kt
+++ app/src/main/java/com/guide/leagueoflegends/myleagueapp/RivenAbilities.kt
@@ -2,11 +2,18 @@ package com.guide.leagueoflegends.myleagueapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_riven_abilities.*
 
 class RivenAbilities : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_riven_abilities)
+
+        val fragmentAdapter = MyPagerAdapter(supportFragmentManager)
+        viewpager_main.adapter = fragmentAdapter
+
+        tabs_main.setupWithViewPager(viewpager_main)
     }
 }
