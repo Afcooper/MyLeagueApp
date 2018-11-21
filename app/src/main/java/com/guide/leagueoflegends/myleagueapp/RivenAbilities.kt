53370f7c1922cbbd36a894019077c7346cc736ff
Alex <speed-demon101@live.com>
2018-11-21 13:06:25 -0600

    I added a gameplay page which will show youtube videos. Right now its bugged and says app stops working when clicking on gameplay. Changed rivenabilities page to just show text for now.

--- app/src/main/java/com/guide/leagueoflegends/myleagueapp/RivenAbilities.kt
+++ app/src/main/java/com/guide/leagueoflegends/myleagueapp/RivenAbilities.kt
@@ -2,10 +2,21 @@ package com.guide.leagueoflegends.myleagueapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore.Video.Thumbnails.VIDEO_ID
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayer.OnInitializedListener
+import com.google.android.youtube.player.YouTubePlayerView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_riven_abilities.*
+import com.google.android.youtube.*
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
 
 class RivenAbilities : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,7 +24,8 @@ class RivenAbilities : AppCompatActivity() {
 
         val fragmentAdapter = MyPagerAdapter(supportFragmentManager)
         viewpager_main.adapter = fragmentAdapter
-
+        //mYoutubePlayerView = findViewById<YouTubePlayerView>(R.id.youtubeButton)
         tabs_main.setupWithViewPager(viewpager_main)
     }
+
 }
---
+++ app/src/main/java/com/guide/leagueoflegends/myleagueapp/YoutubeConfig.java
@@ -0,0 +1,11 @@
+package com.guide.leagueoflegends.myleagueapp;
+
+public class YoutubeConfig {
+    public YoutubeConfig()  {
+
+    }
+    private static final String API_KEY = "AIzaSyD4i2Vm3rNX6y9fRlL7tWpXjP41YZEhN-I";
+    public static String getApiKey()    {
+        return API_KEY;
+    }
+}
